f8e04ba9e98cc5e2d7d8e03300d7acf8dcda19d2
Brian Kim <bk@Brians-MacBook-Air.local>
2015-08-19 18:34:53 -0400

    added a field for timestamp and author

--- schema.sql
+++ schema.sql
@@ -2,5 +2,7 @@ drop table if exists entries;
 create table entries (
   id integer primary key autoincrement,
   title text not null,
+  author text not null,
+  timestamp integer not null,
   'text' text not null
 );
